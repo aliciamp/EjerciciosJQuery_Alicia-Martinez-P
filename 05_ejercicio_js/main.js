c08fdae8f26191ef486f74897d231155cade3d91
“Alicia <nombre@gmail.com>
2018-03-09 21:14:06 +0100

    Corrijo ejercicio 5

--- 05_ejercicio_js/main.js
+++ 05_ejercicio_js/main.js
@@ -7,3 +7,11 @@ $(document).on("click", function(){
     $("#carrete").show()
     $("h1").hide()
 });
+
+$("img").on("mouseover", function(){
+    $(this).css("width", "300px")
+});
+
+$("img").on("mouseout", function(){
+    $(this).css("width", "250px")
+});
